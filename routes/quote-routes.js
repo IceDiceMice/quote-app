7ab32333542ff145a11b80ca87898fb10bf77a91
Dima Olyinyk <prototupe@ukr.net>
2021-12-17 13:21:41 +0200

    another one

--- routes/quote-routes.js
+++ routes/quote-routes.js
@@ -5,29 +5,29 @@ const Quotes = require("../models/quotes-schema");
 const cors = require("cors");
 
 router.get("/", cors(), async (req, res) => {
-  // try {
-  //   const quote = await Quotes.find();
-  //   res.json(quote);
-  // } catch (err) {
-  //   console.log(err);
-  //   res.status(400).send("Error");
-  // }
-  res.json([
-    {
-      _id: "61ac624fc2ce6c6beb50f786",
-      quote:
-        "Perfection is not attainable, but if we chase perfection we can catch excellence..",
-      author: "Vince Lombardi",
-      __v: 0,
-    },
-    {
-      _id: "61aea8783bc58a7ba224ad42",
-      quote:
-        "I keep asking myself these three questions.. What do you have? What do you want? What will you give up?",
-      author: "Jack Ma",
-      __v: 0,
-    },
-  ]);
+  try {
+    const quote = await Quotes.find();
+    res.json(quote);
+  } catch (err) {
+    console.log(err);
+    res.status(400).send("Error");
+  }
+  // res.json([
+  //   {
+  //     _id: "61ac624fc2ce6c6beb50f786",
+  //     quote:
+  //       "Perfection is not attainable, but if we chase perfection we can catch excellence..",
+  //     author: "Vince Lombardi",
+  //     __v: 0,
+  //   },
+  //   {
+  //     _id: "61aea8783bc58a7ba224ad42",
+  //     quote:
+  //       "I keep asking myself these three questions.. What do you have? What do you want? What will you give up?",
+  //     author: "Jack Ma",
+  //     __v: 0,
+  //   },
+  // ]);
 });
 router.post("/add", cors(), async (req, res) => {
   try {
--- server.js
+++ server.js
@@ -23,23 +23,23 @@ app.get("/", (req, res) => res.send("Hello world!"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// app.use((req, res, next) => {
-//   // res.header("Access-Control-Allow-Origin", "*");
-//   // res.header(
-//   //   "Access-Control-Allow-Headers",
-//   //   "Origin, X-Requested-With, Content-Type, Accept"
-//   // );
-//   res.header(
-//     "Access-Control-Allow-Origin",
-//     "https://the-quote-app.herokuapp.com"
-//   );
-//   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-//   res.header(
-//     "Access-Control-Allow-Headers",
-//     "Origin, X-Requested-With, Content-Type, Accept"
-//   );
-//   next();
-// });
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.header(
+    "Access-Control-Allow-Origin",
+    "https://the-quote-app.herokuapp.com"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 app.use(function (req, res, next) {
   if (req.method === "OPTIONS") {
     res.status(204);
